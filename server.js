d42b81498a53d7c476fca73bb62655c280b81b16
hismethod <his.methods@gmail.com>
2018-02-07 00:18:47 -0500

    change port

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     res.send('Hello world');
 });
 
-app.listen(3000, function(req, res){
-    console.log('running');
+app.listen(port, function(req, res){
+    console.log('app is running');
 });
